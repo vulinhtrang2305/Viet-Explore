067e4c1cecb5d012858b4c030d4f75c272c79417
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-20 22:04:04 +0700

    feat: new slice categories

---
+++ fe/store/slices/categorySlice.ts
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchCategories = createAsyncThunk(
+  "categories/fetchCategories",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await axios.get("http://localhost:9999/categories");
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+const categoriesSlice = createSlice({
+  name: "categories",
+  initialState: {
+    categories: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCategories.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.categories = action.payload;
+      })
+      .addCase(fetchCategories.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default categoriesSlice.reducer;
