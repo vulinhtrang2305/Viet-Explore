86eed126219636aa7b0cea69d6cfee5d3b606c09
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-20 22:07:14 +0700

    feat: add suggest slice and add index

--- fe/store/index.tsx
+++ fe/store/index.tsx
@@ -1,9 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import spotReducer from './slices/spotSlice';
+import categoriesReducer from './slices/categorySlice';
+import reviewsReducer from './slices/reviewSlice';
+import usersReducer from './slices/userSlice';
+import suggestsReducer from './slices/suggestSlice';
 
 export const store = configureStore({
     reducer: {
         spots: spotReducer,
+        categories: categoriesReducer,
+        reviews: reviewsReducer,
+        users: usersReducer,
+        suggests: suggestsReducer,
     }
 });
 
