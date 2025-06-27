5dc707c2cc3294ad9d7581f8bc8ac16c426d15b8
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-27 16:05:36 +0700

    chore

--- be/src/controllers/users/users.controller.js
+++ be/src/controllers/users/users.controller.js
@@ -1,5 +1,5 @@
 const User = require("../../models/Users/user.model");
-const { hashMake } = require("../../utils/hash");
+const { hashMake, hashCheck } = require("../../utils/hash");
 
 module.exports = {
     getAllUser: async (req, res) => {
