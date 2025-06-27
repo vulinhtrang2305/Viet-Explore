2063718d8a3c26ef149738a6db120e128c54fd5f
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-27 15:57:07 +0700

    feat: add register in backend

--- be/src/controllers/users/users.controller.js
+++ be/src/controllers/users/users.controller.js
@@ -13,5 +13,36 @@ module.exports = {
         } catch (error) {
             return res.status(500).json({ message: error.message });
         }
-    }
+    },
+
+    register: async (req, res) => {
+        try {
+            const { username, email, password } = req.body;
+            if (!email || !password || !username) {
+                return res.json({
+                    message: "nhập thiếu thông tin vui lòng nhâp lại",
+                });
+            }
+            // validate
+            const userExist = await User.findOne({ email });
+            if (userExist) {
+                return res.status(500).json({ message: "User da ton tai" });
+            }
+
+            const passwordHash = await hashMake(password);
+            const user = await User.create({
+                username,
+                email,
+                password: passwordHash,
+            });
+
+            return res.status(201).json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+            });
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    },
 };
