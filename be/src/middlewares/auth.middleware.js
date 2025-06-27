58ee31c5d03a66c953d3f9d1e84d4e13a36b716c
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-27 16:42:43 +0700

    chore

--- be/src/middlewares/auth.middleware.js
+++ be/src/middlewares/auth.middleware.js
@@ -19,13 +19,13 @@ module.exports = async (req, res, next) => {
         // giai ma token
         const decode = await verifyToken(accessToken)
         if (!decode) {
-            return res.json({ message: "token ko hop le" })
+            return res.json({ message: "Token không hợp lệ" })
         }
 
         const userId = decode?.id;
         const user = await User.findById(userId);
         if (!user) {
-            return res.json({ message: "khong tim thay user" })
+            return res.json({ message: "Không tìm thấy tài khoản này" })
         }
 
         const newUser = {
