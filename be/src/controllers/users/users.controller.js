2d2fc36becd4e45c98e1c671789067731194bc5d
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-29 05:27:59 +0700

    feat: add update user

--- be/src/controllers/users/users.controller.js
+++ be/src/controllers/users/users.controller.js
@@ -18,6 +18,35 @@ module.exports = {
         }
     },
 
+    updateUser: async (req, res) => {
+        try {
+            const userId = req.user.id;
+            const { username, email, phone, address, dob } = req.body;
+
+            const user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({ message: "Người dùng không tồn tại" });
+            }
+
+            user.username = username || user.username;
+            user.email = email || user.email;
+            user.phone = phone || user.phone;
+            user.address = address || user.address;
+            user.dob = dob || user.dob;
+
+            const updatedUser = await user.save();
+
+            const userData = {
+                ...updatedUser._doc,
+                password: "not show",
+            };
+
+            return res.json({ message: "Cập nhật thành công", user: userData });
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    },
+
     register: async (req, res) => {
         try {
             const { username, email, password } = req.body;
