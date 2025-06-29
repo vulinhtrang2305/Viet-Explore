9c7c9313b91017ccee74211669d57488026cba3e
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-29 15:07:50 +0700

    fix be add favourite

--- be/src/controllers/favourites/favourites.controller.js
+++ be/src/controllers/favourites/favourites.controller.js
@@ -15,30 +15,59 @@ module.exports = {
     },
 
     addToFavouriteL: async (req, res) => {
+        // const { userId, spotId } = req.body;
+
+        // try {
+        //     let favourite = await Favourite.findOne({ userId });
+
+        //     if (!favourite) {
+        //         // Nếu chưa có bản ghi nào cho user này thì tạo mới
+        //         favourite = new Favourite({
+        //             userId,
+        //             spotId: [spotId],
+        //         });
+        //     } else {
+        //         // Nếu đã có thì thêm spotId mới vào (tránh trùng lặp)
+        //         if (!favourite.spotId.includes(spotId)) {
+        //             favourite.spotId.push(spotId);
+        //         }
+        //     }
+
+        //     await favourite.save();
+
+        //     return res.status(200).json({
+        //         message: "Đã thêm vào mục ưa thích",
+        //         data: favourite,
+        //     });
+        // } catch (error) {
+        //     return res.status(500).json({ message: error.message });
+        // }
+
         const { userId, spotId } = req.body;
 
         try {
-            const favourite = await Favourite.findOne({ userId });
+            const existingFavourite = await Favourite.findOne({ userId });
 
-            if (!favourite) {
-                // Nếu chưa có bản ghi nào cho user này thì tạo mới
-                favourite = new Favourite({
+            if (!existingFavourite) {
+                const newFavourite = new Favourite({
                     userId,
                     spotId: [spotId],
                 });
+                await newFavourite.save();
+                return res.status(200).json({
+                    message: "Đã thêm vào mục ưa thích",
+                    data: newFavourite,
+                });
             } else {
-                // Nếu đã có thì thêm spotId mới vào (tránh trùng lặp)
-                if (!favourite.spotId.includes(spotId)) {
-                    favourite.spotId.push(spotId);
+                if (!existingFavourite.spotId.includes(spotId)) {
+                    existingFavourite.spotId.push(spotId);
+                    await existingFavourite.save();
                 }
+                return res.status(200).json({
+                    message: "Đã thêm vào mục ưa thích",
+                    data: existingFavourite,
+                });
             }
-
-            await favourite.save();
-
-            return res.status(200).json({
-                message: "Đã thêm vào mục ưa thích",
-                data: favourite,
-            });
         } catch (error) {
             return res.status(500).json({ message: error.message });
         }
