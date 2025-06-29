31d3a330752b5386d3f8b2a3ad6c8a5e88f903d0
Trang Vu <vulinhtrang2305@gmail.com>
2025-06-29 15:01:28 +0700

    add router in be favourite

--- be/src/routers/favouriteApi/favourite.api.js
+++ be/src/routers/favouriteApi/favourite.api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const FavouriteController = require("../../controllers/favourites/favourites.controller");
 
 router.get("/favourites", FavouriteController.getAllFavourite)
+router.post("/favourites/add", FavouriteController.addToFavouriteL)
 
 module.exports = router;
