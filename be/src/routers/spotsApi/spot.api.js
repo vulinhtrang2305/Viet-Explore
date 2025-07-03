f773508d91baf1cca0b5e87d1006d42d38b98d4e
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-04 04:08:35 +0700

    feat: update Spot in BE

--- be/src/routers/spotsApi/spot.api.js
+++ be/src/routers/spotsApi/spot.api.js
@@ -4,5 +4,6 @@ const spotController = require("../../controllers/spots/spots.controller");
 
 router.get("/spots", spotController.getAllSpot)
 router.post("/spots/create", spotController.createSpot)
+router.put("/spots/update/:id", spotController.updateSpot)
 
 module.exports = router;
