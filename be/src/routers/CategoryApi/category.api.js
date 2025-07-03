babac359c0d2500ce5c705fb75543c8091ce9bae
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-04 03:35:32 +0700

    feat: create category BE

--- be/src/routers/CategoryApi/category.api.js
+++ be/src/routers/CategoryApi/category.api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CategoryController = require("../../controllers/categories/categories.controller");
 
 router.get("/categories", CategoryController.getAllCategory)
+router.post("/categories/create", CategoryController.createCategory)
 
 module.exports = router;
