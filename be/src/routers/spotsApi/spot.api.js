af3340fc6f9e5e1e8b6bcbc90879d8bff4cb278c
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-04 03:49:26 +0700

    feat: create spot in BE

--- be/src/routers/spotsApi/spot.api.js
+++ be/src/routers/spotsApi/spot.api.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const spotController = require("../../controllers/spots/spots.controller");
 
 router.get("/spots", spotController.getAllSpot)
+router.post("/spots/create", spotController.createSpot)
 
 module.exports = router;
