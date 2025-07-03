eb56b7b7bfb8ea5462c19409c02ff83b910c822a
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-04 04:17:48 +0700

    feat: delete Spot in BE

--- be/src/routers/spotsApi/spot.api.js
+++ be/src/routers/spotsApi/spot.api.js
@@ -5,5 +5,6 @@ const spotController = require("../../controllers/spots/spots.controller");
 router.get("/spots", spotController.getAllSpot)
 router.post("/spots/create", spotController.createSpot)
 router.put("/spots/update/:id", spotController.updateSpot)
+router.delete("/spots/delete/:id", spotController.deleteSpot)
 
 module.exports = router;
