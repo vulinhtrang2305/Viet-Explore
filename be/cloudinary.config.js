6ca558c4071906b54e4ee4612d324a5fc87c9533
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-20 03:40:15 +0700

    feat: add cloudinary in be

---
+++ be/cloudinary.config.js
@@ -0,0 +1,10 @@
+// cloudinary.config.js
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dd4qz2k1g',
+    api_key: '188465838541653',
+    api_secret: '9dfb-DUgFv5N4oHerNQNgSQfBpI'
+});
+
+module.exports = cloudinary;
