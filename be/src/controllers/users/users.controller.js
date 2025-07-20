c52776c17f58464f6ffc1d49e86ea85f155811fe
Trang Vu <vulinhtrang2305@gmail.com>
2025-07-20 18:28:30 +0700

    chore: user controller

--- be/src/controllers/users/users.controller.js
+++ be/src/controllers/users/users.controller.js
@@ -19,36 +19,6 @@ module.exports = {
         }
     },
 
-    // updateUser: async (req, res) => {
-    //     try {
-    //         const userId = req.params.id;
-    //         const { username, email, phone, address, dob } = req.body;
-
-    //         const user = await User.findById(userId);
-    //         if (!user) {
-    //             return res.status(404).json({ message: "Người dùng không tồn tại" });
-    //         }
-
-    //         user.username = username || user.username;
-    //         user.email = email || user.email;
-    //         user.phone = phone || user.phone;
-    //         user.address = address || user.address;
-    //         user.dob = dob || user.dob;
-
-    //         const updatedUser = await user.save();
-
-    //         const userData = {
-    //             ...updatedUser._doc,
-    //             password: "not show",
-    //         };
-
-    //         return res.json({ message: "Cập nhật thành công", user: userData });
-    //     } catch (error) {
-    //         return res.status(500).json({ message: error.message });
-    //     }
-    // },
-    
-
     updateUser: async (req, res) => {
         try {
             const userId = req.params.id;
